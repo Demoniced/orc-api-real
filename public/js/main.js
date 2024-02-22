bf847a69611f621eff32b540f7d1d345176ee9c2
Peter <Peterinsinga83@gmail.com>
2024-02-21 21:36:42 -0500

    Added Post and Put and Deletes

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
 Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click',deleteSubject)
+    element.addEventListener('click', deleteSubject)
 })
 
 Array.from(thumbText).forEach((element)=>{
@@ -11,24 +11,44 @@ Array.from(thumbText).forEach((element)=>{
 
 async function deleteSubject(){
     const fName = this.parentNode.childNodes[1].innerText
-    const  lName = this.parentNode.childNodes[3].innerText
-    const age = this.parentNode.childNodes[5].innerText
-    const description = this.parentNode.childNodes[7].innerText
+    const lName = this.parentNode.childNodes[3].innerText
     try{
         const response = await fetch('deleteSubject', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                'firstNameS': fName,
-                'lastNameS': lName,
-                'ageS': age,
-                'descriptionS': description
+              'firstNameS': fName,
+              'lastNameS': lName
             })
-    })
+          })
         const data = await response.json()
         console.log(data)
         location.reload()
-    } .catch(err){
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const fName = this.parentNode.childNodes[1].innerText
+    const lName = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[9].innerText)
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'firstNameS': fName,
+              'lastNameS': lName,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
         console.log(err)
     }
 }
--- server.js
+++ server.js
@@ -30,13 +30,40 @@ app.get('/',(req,res)=>{
 })
 //adding one subject to the DB when the form is submitted.
 app.post('/addSubject',(req,res)=>{
-    db.collection('subjects').insertOne({firstName: req.body.firstName,lastName: req.body.lastName,age: req.body.age,description: req.body.description})
+    db.collection('subjects').insertOne({firstName: req.body.firstName,lastName: req.body.lastName,age: req.body.age,description: req.body.description,likes: 0})
     .then(result =>{
         console.log('Subject Added')
         //refreshes page after the POST
         res.redirect('/')
     })
     .catch(error=>console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('subjects').updateOne({firstName: request.body.firstNameS, lastName: request.body.lastNameS,likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        sort: {_id: 1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteSubject', (request, response) => {
+    db.collection('subjects').deleteOne({firstName: request.body.firstNameS})
+    .then(result => {
+        console.log('Subject Deleted')
+        response.json('Subject Deleted')
+    })
+    .catch(error => console.error(error))
+
 })
 
 app.listen(PORT, ()=>{
